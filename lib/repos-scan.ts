313d08c1cb3e6134f103decf589e3f7ba7e81efa
Min Kim <minkimcello@gmail.com>
2024-11-30 14:41:21 -0500

    Scan and export scan report sarif files

--- lib/repos-scan.ts
+++ lib/repos-scan.ts
@@ -1,5 +1,17 @@
 import { useLogger } from "fetcher-lib/useLogger.ts";
-import { call } from "npm:effection@4.0.0-alpha.3";
+import { exists } from "jsr:@std/fs@1.0.4/exists";
+import { call, type Operation, } from "npm:effection@4.0.0-alpha.3";
+import fs from "node:fs";
+import { promisify } from "node:util";
+
+function* mkdir(
+  path: fs.PathLike,
+  options?: fs.MakeDirectoryOptions & {
+    recursive: true;
+  },
+): Operation<string | undefined> {
+  return yield* call(() => promisify(fs.mkdir)(path, options));
+}
 
 export function* scanRepositories(localRepositoryPaths: {
   path: string,
@@ -18,17 +30,28 @@ export function* scanRepositories(localRepositoryPaths: {
     throw new Error("Unable to find trivy");
   }
 
-  for (const { path, name } of localRepositoryPaths) {
+  const resultsDirectory = `${Deno.cwd()}/.cache/sarif/`;
+  const resultsDirectoryExists = yield* call(async () => await exists(resultsDirectory));
+  if (!resultsDirectoryExists) {
+    yield* mkdir(resultsDirectory);
+  }
+
+  for (const [index, { path, name }] of localRepositoryPaths.entries()) {
+    logger.info('Running trivy scan for', name, `${index+1}/${localRepositoryPaths.length}`);
     const scan = new Deno.Command("trivy", {
       args: [
-        "trivy",
         "fs",
-        "--scanners vuln,secret",
-        "--format sarif",
-        `--output ${path}/../../sarif/${name}.sarif`,
+        "--scanners", "vuln,secret",
+        "--format", "sarif",
+        `--output`, `${path}/../../sarif/${name}.sarif`,
         path,
       ],
     });
-    yield* call(async () => await scan.output());
+    yield* call(async () => {
+      const result = await scan.output();
+      if (result.code !== 0) {
+        logger.info(new TextDecoder().decode(result.stdout), new TextDecoder().decode(result.stderr))
+      }
+    });
   }
 }
