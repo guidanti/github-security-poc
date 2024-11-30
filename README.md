00d90f5796b24dcc13d9fffdcc62bbe11800a103
Taras Mankovski <taras@frontside.com>
2024-11-30 16:01:13 -0500

    Uploading to S3 bucket using deno script

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Getting Started
 
-## Minio
+## MinIO
 
 1. [Install Minio client](https://min.io/docs/minio/linux/reference/minio-mc.html#install-mc)
 2. Configure your `mc` client to access Minio from docker 
    `mc alias set myminio http://127.0.0.1:9000 minioadmin minioadmin`
+
---
+++ lib/s3.ts
@@ -0,0 +1,48 @@
+import {
+  PutObjectCommand,
+  type PutObjectCommandInput,
+  type PutObjectCommandOutput,
+  S3Client,
+  type S3ClientConfig,
+} from "npm:@aws-sdk/client-s3@3.701.0";
+import {
+  call,
+  createContext,
+  type Operation,
+  resource,
+} from "npm:effection@4.0.0-alpha.3";
+
+export { type S3ClientConfig } from "npm:@aws-sdk/client-s3@3.701.0";
+
+const S3ClientContext = createContext<S3Client>("s3-client");
+
+
+function createS3Client(config: S3ClientConfig) {
+  return resource<S3Client>(function* (provide) {
+    let s3: S3Client | undefined;
+    try {
+      s3 = new S3Client(config);
+      yield* provide(s3);
+    } finally {
+      s3?.destroy();
+    }
+  });
+}
+
+export function* initS3Client(config: S3ClientConfig) {
+  const s3 = yield* createS3Client(config);
+
+  return yield* S3ClientContext.set(s3);
+}
+
+export function* useS3Client(): Operation<S3Client> {
+  return yield* S3ClientContext.expect();
+}
+
+export function* putObject(
+  input: PutObjectCommandInput,
+): Operation<PutObjectCommandOutput> {
+  const s3 = yield* useS3Client();
+
+  return yield* call(() => s3.send(new PutObjectCommand(input)));
+}
