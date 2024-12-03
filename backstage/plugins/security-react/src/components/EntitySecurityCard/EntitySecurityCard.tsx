fba93de6dab6f0dfff3d3fa94cca78f29d444eea
Min Kim <minkimcello@gmail.com>
2024-12-03 18:13:57 -0500

    Set latest commit as default and add levels summary counts

--- backstage/plugins/security-react/src/components/EntitySecurityCard/EntitySecurityCard.tsx
+++ backstage/plugins/security-react/src/components/EntitySecurityCard/EntitySecurityCard.tsx
@@ -19,8 +19,10 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
-import { SecurityReportTable } from '../SecurityReportTable';
+import { SecurityReportTable, type SarifResult } from '../SecurityReportTable';
 
 const useStyles = makeStyles(theme => ({
   gridItemCard: {
@@ -31,13 +33,32 @@ const useStyles = makeStyles(theme => ({
   cardHeader: {
     paddingBottom: theme.spacing(1),
   },
+  cardSummary: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: theme.spacing(10),
+  },
   cardContent: {
     display: 'flex',
     flexDirection: 'column',
   },
   formControl: {
     width: theme.spacing(23),
-    marginBottom: theme.spacing(2),
+  },
+  summaryValue: {
+    fontWeight: 'bold',
+    overflow: 'hidden',
+    lineHeight: '24px',
+    wordBreak: 'break-word',
+  },
+  summaryLabel: {
+    color: theme.palette.text.secondary,
+    textTransform: 'uppercase',
+    fontSize: '10px',
+    fontWeight: 'bold',
+    letterSpacing: 0.5,
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
   },
 }));
 
@@ -112,6 +133,7 @@ export const EntitySecurityCard = () => {
           per_page: 7,
         },
       );
+      setCommit(repoCommits.data[0].sha);
       return repoCommits.data.map(commit => commit.sha);
     }
     return [];
@@ -141,10 +163,24 @@ export const EntitySecurityCard = () => {
           ],
         },
       } = await sarifs.json();
+
+      const levels = JSON.parse(results)
+        .map((result: SarifResult) => result.level)
+        .reduce((acc: { [key: string]: number }[], level: string) => {
+          const existingItem = acc.find(item => item[level] !== undefined);
+          if (existingItem) {
+            existingItem[level] += 1;
+          } else {
+            acc.push({ [level]: 1 });
+          }
+          return acc;
+        }, [] as { [key: string]: number }[]);
+
       return {
         github_sha,
         tool_name,
         results: JSON.parse(results),
+        levels,
       };
     }
     return;
@@ -163,7 +199,7 @@ export const EntitySecurityCard = () => {
     <Card className={classes.gridItemCard}>
       <CardHeader title={'Security'} className={classes.cardHeader} />
       <Divider />
-      <CardContent className={classes.cardContent}>
+      <CardContent className={classes.cardSummary}>
         <FormControl variant="outlined" className={classes.formControl}>
           <InputLabel>Commits</InputLabel>
           <Select
@@ -185,6 +221,12 @@ export const EntitySecurityCard = () => {
             })}
           </Select>
         </FormControl>
+        { commit && sarifs.value ? sarifs.value.levels.map((level: { [key: string]: number }) => {
+          const [label, count] = Object.entries(level)[0];
+          return <AboutField label={label} value={`${count}`} />
+        }) : null}
+      </CardContent>
+      <CardContent className={classes.cardContent}>
         {commit && sarifs.value ? (
           <SecurityReportTable results={sarifs.value.results} />
         ) : null}
@@ -223,3 +265,26 @@ const SHA_QUERY = `
     }
   }
 `;
+
+export interface AboutFieldProps {
+  label: string;
+  value?: string;
+  gridSizes?: Record<string, number>;
+  className?: string;
+}
+
+function AboutField(props: AboutFieldProps) {
+  const { label, value, gridSizes, className } = props;
+  const classes = useStyles();
+
+  return (
+    <Grid item {...gridSizes} className={className}>
+      <Typography variant="h2" className={classes.summaryLabel}>
+        {label}
+      </Typography>
+      <Typography variant="body2" className={classes.summaryValue}>
+        {value || `unknown`}
+      </Typography>
+    </Grid>
+  );
+}
